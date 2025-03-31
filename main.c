ae2ff29489a161308c8000dfd86996afc3825d7e
Lolo <loic.philippe@epitech.eu>
2025-03-31 10:33:22 +0200

    test

--- main.c
+++ main.c
@@ -7,10 +7,5 @@
 
 int main(void)
 {
-    for (int i = 0; i < 10; i++)
-        for (int j = 0; j < 10; j++)
-            for (int k = 0; k < 10; k++)
-                if (i != j && i != k && j != k)
-                    i = i + 1;
     return (0);
 }
